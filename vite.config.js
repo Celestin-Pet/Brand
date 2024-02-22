1c08ab7ed4f3fce96d636f34614064b3bb4d2a74
Celestin-Pet <celestin.np@gmail.com>
2024-02-22 19:30:52 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,9 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { readdirSync } from 'fs';
-import { join } from 'path';
 
-export default defineConfig({
+export default {
   base: "/portfolio",
   plugins: [react()],
   build: {
     outDir: "build",
   },
-  optimizeDeps: {
-    exclude: ['path', 'fs']
-  },
-  async configureServer(server) {
-    const publicFiles = readdirSync(join(__dirname, 'public'))
-    const srcFiles = readdirSync(join(__dirname, 'src'))
-    const allFiles = [...publicFiles, ...srcFiles]
-    allFiles.forEach(file => {
-      server.middlewares.use('/' + file, (req, res, next) => {
-        const filePath = join(__dirname, 'public', file)
-        res.setHeader('Content-Type', 'text/html')
-        res.end(readdirSync(filePath))
-      })
-    })
-  }
-});
+};
